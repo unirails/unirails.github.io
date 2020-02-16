40f1a0193545de436b283073bd7bdfe547369cef
N8te <natonting@gmail.com>
2020-02-16 22:49:01 +0800

    Bugfix to announcement strings not showing \n

--- generators/pa/pa-generator.js
+++ generators/pa/pa-generator.js
@@ -7,7 +7,7 @@ let SBC = "技術城 SBC";
 let SHG = "深卡關閘 Shimakaze Gateway";
 let HOY = "何遙 Ho Yiu";
 let SHL = "深卡綫 Shimakaze Line"
-let OHA = "Oleung Hit Airplane"
+let OHA = "梁主席萬歲 Oleung Hit Airplane"
 
 let shlStations = [STA,DAK,SHI,SHC,SHN,SBC,SHG,HOY,OHA];
 let shlStationCount = shlStations.length;
@@ -17,6 +17,9 @@ for (let i = 0; i < shlStationCount; i++) {
   console.log(shlStations[i]);
   let existingHTML = document.getElementById("stationList").innerHTML;
   let stationNameHeader = "<h2>" + shlStations[i] + "</h2>";
-  let announcementString = '<div class="form-group"><textarea class="form-control"rows="10" readonly>\ntellraw @a ["",{"text":"\n"},{"text":"----=[ ","bold":true,"color":"gold"},{"text":"' + SHL + '","color":"red"},{"text":" ]=----","bold":true,"color":"gold"},{"text":"\n"},{"text":"▽ ","color":"dark_gray"},{"text":"' + shlStations[i-1] + '","color":"dark_gray"},{"text":"\n"},{"text":"▼ ","color":"green"},{"text":"' + shlStations[i] +'","bold":true,"color":"yellow"},{"text":"\n"},{"text":"▽ ","color":"green"},{"text":"' + shlStations[i+1] + '","color":"gray"},{"text":"\n"},{"text":"▽ ","color":"green"},{"text":"' + shlStations[i+2] + '","color":"gray"},{"text":"\n"}]</textarea></div>'
+  if (shlStations[i-1] === null) {
+    console.log("no previous station");
+  };
+  let announcementString = '<div class="form-group"><textarea class="form-control"rows="5" readonly>\ntellraw @a ["",{"text":"&bsol;n"},{"text":"----=[ ","bold":true,"color":"gold"},{"text":"' + SHL + '","color":"red"},{"text":" ]=----","bold":true,"color":"gold"},{"text":"&bsol;n"},{"text":"▽ ","color":"dark_gray"},{"text":"' + shlStations[i-1] + '","color":"dark_gray"},{"text":"&bsol;n"},{"text":"▼ ","color":"green"},{"text":"' + shlStations[i] +'","bold":true,"color":"yellow"},{"text":"&bsol;n"},{"text":"▽ ","color":"green"},{"text":"' + shlStations[i+1] + '","color":"gray"},{"text":"&bsol;n"},{"text":"▽ ","color":"green"},{"text":"' + shlStations[i+2] + '","color":"gray"},{"text":"&bsol;n"}]</textarea></div>'
   document.getElementById("stationList").innerHTML = existingHTML + stationNameHeader + announcementString;
 }
