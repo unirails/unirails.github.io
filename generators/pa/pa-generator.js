c10cd4909d62fd593d691f388960891817bc0919
N8te <natonting@gmail.com>
2020-02-16 23:43:22 +0800

    Added down line functionality

--- generators/pa/pa-generator.js
+++ generators/pa/pa-generator.js
@@ -1,3 +1,5 @@
+let SHL = "深卡綫 Shimakaze Line"
+
 let STA = "飛渡市 Stagecoach";
 let DAK= "達卡 Dat Ka";
 let SHI = "深卡島 Shimakaze";
@@ -6,20 +8,30 @@ let SHN = "沙草灣 Savanna Bay";
 let SBC = "技術城 SBC";
 let SHG = "深卡關閘 Shimakaze Gateway";
 let HOY = "何遙 Ho Yiu";
-let SHL = "深卡綫 Shimakaze Line"
 let OHA = "梁主席萬歲 Oleung Hit Airplane"
 
 let shlStations = [STA,DAK,SHI,SHC,SHN,SBC,SHG,HOY,OHA];
 let shlStationCount = shlStations.length;
-// let stationListDisplay = document.getElementById("stationList").innerHTML
+// let stationListDisplay = document.getElementById("upStationList").innerHTML
 
 for (let i = 0; i < shlStationCount; i++) {
   console.log(shlStations[i]);
-  let existingHTML = document.getElementById("stationList").innerHTML;
+  let existingHTML = document.getElementById("upStationList").innerHTML;
   let stationNameHeader = "<h2>" + shlStations[i] + "</h2>";
   if (shlStations[i-1] === null) {
     console.log("no previous station");
   };
   let announcementString = '<div class="form-group"><textarea class="form-control"rows="5" onclick="this.focus();this.select()" id="station-' + i + '" readonly>\ntellraw @a ["",{"text":"&bsol;n"},{"text":"----=[ ","bold":true,"color":"gold"},{"text":"' + SHL + '","color":"red"},{"text":" ]=----","bold":true,"color":"gold"},{"text":"&bsol;n"},{"text":"▽ ","color":"dark_gray"},{"text":"' + shlStations[i-1] + '","color":"dark_gray"},{"text":"&bsol;n"},{"text":"▼ ","color":"green"},{"text":"' + shlStations[i] +'","bold":true,"color":"yellow"},{"text":"&bsol;n"},{"text":"▽ ","color":"green"},{"text":"' + shlStations[i+1] + '","color":"gray"},{"text":"&bsol;n"},{"text":"▽ ","color":"green"},{"text":"' + shlStations[i+2] + '","color":"gray"},{"text":"&bsol;n"}]</textarea></div></br>'
-  document.getElementById("stationList").innerHTML = existingHTML + stationNameHeader + announcementString;
+  document.getElementById("upStationList").innerHTML = existingHTML + stationNameHeader + announcementString;
+}
+
+for (let i = shlStationCount-1; i > -1; i--) {
+  console.log(shlStations[i]);
+  let existingHTML = document.getElementById("dnStationList").innerHTML;
+  let stationNameHeader = "<h2>" + shlStations[i] + "</h2>";
+  if (shlStations[i-1] === null) {
+    console.log("no previous station");
+  };
+  let announcementString = '<div class="form-group"><textarea class="form-control"rows="5" onclick="this.focus();this.select()" id="station-' + i + '" readonly>\ntellraw @a ["",{"text":"&bsol;n"},{"text":"----=[ ","bold":true,"color":"gold"},{"text":"' + SHL + '","color":"red"},{"text":" ]=----","bold":true,"color":"gold"},{"text":"&bsol;n"},{"text":"▽ ","color":"dark_gray"},{"text":"' + shlStations[i+1] + '","color":"dark_gray"},{"text":"&bsol;n"},{"text":"▼ ","color":"green"},{"text":"' + shlStations[i] +'","bold":true,"color":"yellow"},{"text":"&bsol;n"},{"text":"▽ ","color":"green"},{"text":"' + shlStations[i-1] + '","color":"gray"},{"text":"&bsol;n"},{"text":"▽ ","color":"green"},{"text":"' + shlStations[i-2] + '","color":"gray"},{"text":"&bsol;n"}]</textarea></div></br>'
+  document.getElementById("dnStationList").innerHTML = existingHTML + stationNameHeader + announcementString;
 }
