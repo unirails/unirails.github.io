95d127369da82662246c020af20395d7c97bb35c
N8te <natonting@gmail.com>
2020-03-03 20:18:53 +0800

    Updated system map page to become more interactive.

---
+++ sym/script.js
@@ -0,0 +1,19 @@
+function sizeContent() {
+  var navHeight = document.getElementsByTagName('nav')[0].clientHeight;
+  var screenHeight = document.getElementsByTagName('body')[0].clientHeight;
+  var screenWidth = document.getElementsByTagName('body')[0].clientWidth;
+  // document.getElementById('image').style.height = screenHeight-navHeight + "px";
+  // document.getElementById('image').style.width = screenWidth + "px";
+  document.getElementsByClassName('iv-container')[0].style.height = screenHeight-navHeight + "px";
+  document.getElementsByClassName('iv-container')[0].style.width = screenWidth + "px";
+  document.getElementsByClassName('iv-image')[0].style.height = screenHeight-navHeight + "px";
+  // document.getElementsByClassName('iv-image')[0].style.width = screenWidth + "px";
+}
+
+const image = document.querySelector('#image');
+const viewer = new ImageViewer(image);
+viewer.load('sym.svg', 'sym.svg');
+viewer.zoom(100, { x: 500, y: 500 });
+// document.getElementById("image").classList.add('.img-fluid');
+
+sizeContent();
