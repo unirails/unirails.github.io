d64daa829c68d038dda1ce6d3d5cf9309798dfda
N8te <natonting@gmail.com>
2020-04-17 15:55:08 +0800

    Minor fixes and optimisation

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// console.log(navHeight);
-// console.log(screenHeight);
-
-function sizeContent() {
-  var navHeight = document.getElementsByTagName('nav')[0].clientHeight;
-  var screenHeight = document.getElementsByTagName('body')[0].clientHeight;
-  document.getElementsByClassName('content')[0].style.height = screenHeight-navHeight + "px";
-}
-
 $(document).ready(function(){
   $('#maincontent').fadeIn();
 })
